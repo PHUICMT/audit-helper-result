d435d24b7190390f3cdbfcb9aa72a1546c85195c
PHUICMT <phuicmt@odds.team>
2023-07-07 07:34:40 +0700

    update port upload

--- src/services/CSVProvider.js
+++ src/services/CSVProvider.js
@@ -74,7 +74,7 @@ export async function JsonToCSV(json_data, callback) {
 }
 
 export async function CSVToApi(csv_data, callback) {
-  const apiUrl = "http://localhost:5000/upload";
+  const apiUrl = "http://localhost/upload";
   const blob = new Blob([csv_data], { type: 'text/csv' });
   const csv_file = new File([blob], 'data.csv', { type: 'text/csv' });
 
